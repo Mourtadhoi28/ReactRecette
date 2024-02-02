3cee0a710b2ca662bf0d54aaba9bf90a33faaec8
Mmadi <m.mourtadhoi@it-students.fr>
2024-02-02 15:50:08 +0100

    initiation Page accueil

--- App.js
+++ App.js
@@ -1,22 +1,37 @@
-
 import * as React from 'react';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-
-export default function App() {
+function HomeScreen() {
   return (
-    <NavigationContainer>{/* Rest of your app code */
-    
-    }</NavigationContainer>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>"Page d'accueil" pour recette de coktail</Text>
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function DetailsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteStackNavigator="Home">
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Recette Coktail' }} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
+
